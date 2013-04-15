c0c6d552f21601a4d02649fcb233417b9cd2483a
Jonathan Tavares <the.entomb@gmail.com>
2013-04-15 22:58:41 +0100

    google stuff

--- info.php
+++ info.php
@@ -103,5 +103,18 @@
 
 
         </div>
+        <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37723000-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     </body>
 </html>
