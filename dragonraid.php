ff37663899f1f5d32636e30cab9512dd46575b9c
Jonathan Tavares <the.entomb@gmail.com>
2013-04-24 00:33:44 +0100

    temporary overwrite of DK and W class sprites

--- dragonraid.php
+++ dragonraid.php
@@ -698,6 +698,10 @@
             $gender = "male";
             $class = $post->class;
             $post_id = $post->id;
+            
+            //this is a temp overwrite
+            if($class=='DK') $class = 'K';
+            if($class=='W')  $class = 'H';
 
             // 64 variations
             $range = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",'0','1','2','3','4','5','6','7','8','9','+','/');
